5d336388351b651a5ed257de296cf72a4bb79dab
Toon Daelman <toon.daelman@engagor.com>
2019-11-06 15:42:43 +0100

    ✨ Reply to incoming messages with a "for approval" post
    We'll reply to incoming messages with a post that's sent for approval.
    This didn't work with the api on app, because there's an issue with
    posting to twitter. Therefor we needed to overwrite the URL to a staging
    url (see changes in Dependency Injection Container).
    
    Co-authored-by: Hans Ott <hansott@hotmail.be>
    Co-authored-by: Mircea Damoc <mircea.damoc@gmail.com>
    Co-authored-by: Ben Serlippens <ben.serlippens@gmail.com>

--- public/index.php
+++ public/index.php
@@ -9,10 +9,14 @@ $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
 $dotenv->load();
 
 $container = new Container();
-$container->set(Engagor\Authentication::class, function () {
-    $httpRequestFactory = new Nyholm\Psr7\Factory\Psr17Factory();
-    $httpClient = new Buzz\Client\Curl($httpRequestFactory);
-
+$container->set('http-factory', function () {
+    return new Nyholm\Psr7\Factory\Psr17Factory();
+});
+$container->set(Psr\Http\Client\ClientInterface::class, function (Container $container) {
+    return new Buzz\Client\Curl($container->get('http-factory'));
+});
+$container->set(Engagor\Authentication::class, function (Container $container) {
+    $httpRequestFactory = $container->get(Psr\Http\Client\ClientInterface::class);
     $clientId = getenv('CLIENT_ID');
     $clientSecret = getenv('CLIENT_SECRET');
 
@@ -24,6 +28,19 @@ $container->set(Engagor\Authentication::class, function () {
     );
 
     return $authentication;
+});
+$container->set(Engagor\Client::class, function (Container $container) {
+    if (file_exists(__DIR__ . '/../token') === false) {
+        throw new RuntimeException('No tokens file, please authenticate first.');
+    }
+
+    $tokens = unserialize(file_get_contents(__DIR__ . '/../token'));
+
+    return new Engagor\Client(
+        new Demo\ClientRouteToStaging($container->get(Psr\Http\Client\ClientInterface::class), 'api.toon.neo.engagor.com'),
+        $container->get('http-factory'),
+        $tokens
+    );
 });
 
 $app = Bridge::create($container);
--- src/WebhooksController.php
+++ src/WebhooksController.php
@@ -2,20 +2,59 @@
 
 namespace Demo;
 
+use Engagor\Client;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 final class WebhooksController
 {
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
     public function webhooks(Request $request, Response $response)
     {
+        $rawMention = $request->getBody()->getContents();
+
         file_put_contents(
             __DIR__ . '/../logs.txt',
-            $request->getBody()->getContents(),
+            $rawMention,
             FILE_APPEND
         );
 
-        $response->getBody()->write('hello');
+        $mention = json_decode($rawMention, true);
+        $topicId = $mention['topic']['id'];
+        $id = $mention['id'];
+        $permalink = $mention['permalink'];
+        $messageType = $mention['message']['type'];
+        $service = $mention['source']['service'];
+        $accountId = preg_replace('/^.*messages\/(\d+)\/.*$/', '$1', $permalink);
+        $profileId = preg_replace('/^.*twitter\.com\/(\d+)\/dm.*$/', '$1', $mention['source']['url']);
+        $authorId = $mention['author']['id'];
+
+        $result = $this->client->publish(
+            $accountId,
+            "privatemessage",
+            [
+                [
+                    'type' => $service,
+                    'service_id' => $profileId,
+                ]
+            ],
+            [
+                $authorId,
+            ],
+            'Move along',
+            'Hi there!',
+            'awaitingapproval',
+            null,
+            $topicId,
+            $id
+        );
+        error_log(print_r($result, true));
 
         return $response;
     }
