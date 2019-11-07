e7fb623c3f3826012c44920c9ec7d643256f7c5a
Toon Daelman <toon.daelman@engagor.com>
2019-11-07 10:09:32 +0100

    ♻️  Extract container config to config file
    Co-authored-by: Hans Ott <hansott@hotmail.be>
    Co-authored-by: Mircea Damoc <mircea.damoc@gmail.com>
    Co-authored-by: Ben Serlippens <ben.serlippens@gmail.com>

---
+++ config/container.php
@@ -0,0 +1,41 @@
+<?php
+
+use DI\Container;
+
+$container = new Container();
+$container->set('http-factory', function () {
+    return new Nyholm\Psr7\Factory\Psr17Factory();
+});
+$container->set(Psr\Http\Client\ClientInterface::class, function (Container $container) {
+    return new Buzz\Client\Curl($container->get('http-factory'));
+});
+$container->set(Engagor\Authentication::class, function (Container $container) {
+    $httpClient = $container->get(Psr\Http\Client\ClientInterface::class);
+    $httpRequestFactory = $container->get('http-factory');
+    $clientId = getenv('CLIENT_ID');
+    $clientSecret = getenv('CLIENT_SECRET');
+
+    $authentication = new Engagor\Authentication(
+        $httpClient,
+        $httpRequestFactory,
+        $clientId,
+        $clientSecret
+    );
+
+    return $authentication;
+});
+$container->set(Engagor\Client::class, function (Container $container) {
+    if (file_exists(__DIR__ . '/../token') === false) {
+        throw new RuntimeException('No tokens file, please authenticate first.');
+    }
+
+    $tokens = unserialize(file_get_contents(__DIR__ . '/../token'));
+
+    return new Engagor\Client(
+        $container->get(Psr\Http\Client\ClientInterface::class),
+        $container->get('http-factory'),
+        $tokens
+    );
+});
+
+return $container;
--- public/index.php
+++ public/index.php
@@ -1,47 +1,13 @@
 <?php
 
 use DI\Bridge\Slim\Bridge;
-use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
 $dotenv->load();
 
-$container = new Container();
-$container->set('http-factory', function () {
-    return new Nyholm\Psr7\Factory\Psr17Factory();
-});
-$container->set(Psr\Http\Client\ClientInterface::class, function (Container $container) {
-    return new Buzz\Client\Curl($container->get('http-factory'));
-});
-$container->set(Engagor\Authentication::class, function (Container $container) {
-    $httpRequestFactory = $container->get(Psr\Http\Client\ClientInterface::class);
-    $clientId = getenv('CLIENT_ID');
-    $clientSecret = getenv('CLIENT_SECRET');
-
-    $authentication = new Engagor\Authentication(
-        $httpClient,
-        $httpRequestFactory,
-        $clientId,
-        $clientSecret
-    );
-
-    return $authentication;
-});
-$container->set(Engagor\Client::class, function (Container $container) {
-    if (file_exists(__DIR__ . '/../token') === false) {
-        throw new RuntimeException('No tokens file, please authenticate first.');
-    }
-
-    $tokens = unserialize(file_get_contents(__DIR__ . '/../token'));
-
-    return new Engagor\Client(
-        new Demo\ClientRouteToStaging($container->get(Psr\Http\Client\ClientInterface::class), 'api.toon.neo.engagor.com'),
-        $container->get('http-factory'),
-        $tokens
-    );
-});
+$container = require __DIR__ . '/../config/container.php';
 
 $app = Bridge::create($container);
 
